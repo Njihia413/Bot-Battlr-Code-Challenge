592380cc58ca8c50710161de9e15983069a4cd8a
Maureen Njihia <maureennjihia468@gmail.com>
2022-09-30 20:11:06 +0300

    Fetch and render bot profiles to the DOM

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import BotCard from "./BotCard";
 
 function BotCollection() {
   // Your code here
+  const botsUrl = " http://localhost:8002/bots";
+  const[botsInfo, setBotsInfo] = useState([]);
+
+  //Fetch Bots
+  useEffect(()=> {
+    fetch(`${botsUrl}`)
+    .then(response => response.json())
+    .then(bots => {
+      const botsList = bots.map((bot) => <BotCard key={bot.id} bot={bot}/>)
+      setBotsInfo(botsList)
+    });
+  }, [])
+
+
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
         Collection of all bots
+        {botsInfo}
       </div>
     </div>
   );
