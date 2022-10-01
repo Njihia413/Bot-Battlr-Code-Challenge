c6ff27a31a880c605c568cd3a8c8eeb3102b8571
Maureen Njihia <maureennjihia468@gmail.com>
2022-10-02 00:26:46 +0300

    Sorting functionality implemented

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 import BotCard from "./BotCard";
 import BotSpecs from "./BotSpecs";
+import SortBar from "./SortBar";
 
 
 function BotsPage() {
@@ -11,7 +12,9 @@ function BotsPage() {
   const [bots, setBots] = useState([]);
   const [botsListed, setBotsListed] = useState([]);
   const [showBotSpecs, setShowBotSpecs] = useState(null);
-  const [filteredBots, setFilteredBots] = useState([])
+  const [filteredBots, setFilteredBots] = useState([]);
+  const [showSortBar, setShowSortBar] = useState(true) //Initially visible
+  const sortStrategy = useRef({ health: 1, damage: 1, armor: 1 })
 
   //Fetch Bots
   useEffect(()=> {
@@ -72,14 +75,41 @@ function BotsPage() {
         break;
 
       case "show-all-bots":
-        setShowBotSpecs(null)
+        setShowBotSpecs(null);
+        setShowSortBar(true)
         break;
   
       case "show-bot-specs":
-        setShowBotSpecs(bot)
+        setShowBotSpecs(bot);
+        setShowSortBar(false);
     }
   }
 
+  //Sorting Functionality
+  function sortBots(data, sortBy){
+    data.sort((a, b) => {
+      if(a[sortBy] > b[sortBy]){
+        return sortStrategy.current[sortBy] * 1
+      }else if(a[sortBy] < b[sortBy]){
+        return sortStrategy.current[sortBy] * -1
+      } else {
+        return 0
+      }
+    })
+
+    return data
+  }
+
+
+  function updateSortStrategy(sortBy){
+    sortStrategy.current[sortBy] *= -1 //if it was ascending, make it descending and vice versa
+  }
+
+
+  function handleSortAction(sortBy){
+    updateSortStrategy(sortBy)
+    setFilteredBots(sortBots([...filteredBots], sortBy))
+  }
 
   function botsList (botsArray) {
     return botsArray.map(bot => <BotCard key={bot.id} bot={bot} handleBotActionClick={handleBotActionClick} /> )
@@ -88,6 +118,7 @@ function BotsPage() {
   return (
     <div>
       <YourBotArmy botsListed={botsList(botsListed)} />
+      {showSortBar ? <SortBar handleSortAction={handleSortAction}/> : <div></div>}
       {showBotSpecs ? <BotSpecs bot={showBotSpecs} handleBotActionClick={handleBotActionClick} alreadyListedBot={alreadyListedBot(showBotSpecs)} /> : <BotCollection filteredBots={botsList(filteredBots)} />}
     </div>
   )
